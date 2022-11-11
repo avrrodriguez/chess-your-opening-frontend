532d18106f2b0f890266ca15fc9ae59d20c0eea6
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-11-11 14:00:26 -0500

    Changes studies so that public studies are displayed separately from private studies

--- src/Studies.jsx
+++ src/Studies.jsx
@@ -14,14 +14,42 @@ export function Studies() {
   useEffect(handleStudiesIndex, []);
 
   return (
-    <div>
-      <h1 className="d-flex justify-content-center">My Studies</h1>
-
-      {studies.map((study) => (
-        <div key={study.id}>
-          <Link to={`/studies/${study.id}`}>yas yas y as</Link>
+    <div className="container">
+      <div className="d-flex justify-content-between">
+        <div className="border ps-5 pe-5" id="studies-public">
+          <h1 className="d-flex justify-content-center">Public Studies</h1>
+          {studies.map((study) =>
+            study.public ? (
+              <div className="mb-2">
+                <h3>{study.opening.name}</h3>
+                <img src={study.opening.image_url} style={{ width: "311px", height: "311px" }} />
+                <div>
+                  <Link to={`/studies/${study.id}`}>yas yas y as</Link>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )
+          )}
         </div>
-      ))}
+        <div className="border ps-5 pe-5" id="studies-private">
+          <h1 className="d-flex justify-content-center">My Studies</h1>
+
+          {studies.map((study) =>
+            study.public ? (
+              <></>
+            ) : (
+              <div className="mb-3 mt-2">
+                <h3>{study.opening.name}</h3>
+                <img src={study.opening.image_url} style={{ width: "311px", height: "311px" }} />
+                <div>
+                  <Link to={`/studies/${study.id}`}>yas yas y as</Link>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 }
